c2248c249e2cec0b5a04d4421f28ecaa85831ab7
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-02-23 14:06:23 +0530

    fix: fixed the application stuck on loading issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Add this import
 import 'features/parcel_feed/presentation/screens/parcel_feed_screen.dart';
 import 'core/theme/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
